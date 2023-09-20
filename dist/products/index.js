c99213b53d2a5440e22611a386cbebc5ce5b0d0f
Khoa Le Rikkei <116610704+khoalyRikkei@users.noreply.github.com>
2023-09-20 18:43:17 +0700

    CRUD

--- dist/products/index.js
+++ dist/products/index.js
@@ -1,110 +1,101 @@
 "use strict";
-class Car {
-    constructor(brand, price, name, color, category) {
-        this.brand = brand;
-        this.category = category;
-        this.color = color;
-        this.price = price;
-        this.name = name;
-    }
-    get getName() {
-        return this.name;
-    }
-    get getPrice() {
-        return this.price;
-    }
-    get getBrand() {
-        return this.brand;
-    }
-    get getColor() {
-        return this.color;
-    }
-    get getCategory() {
-        return this.category;
-    }
-    set setName(name) {
-        this.name = name;
-    }
-    set setPrice(price) {
-        this.price = price;
-    }
-    set setBrand(brand) {
-        this.brand = brand;
-    }
-    set setColor(color) {
-        this.color = color;
-    }
-    set setCategory(category) {
-        this.category = category;
-    }
-    inputData() {
-        const name = prompt("Nhập tên xe", this.name);
-        const price = Number(prompt("Nhập tên giá", this.price.toString()));
-        const color = prompt("Nhập tên màu");
-        const brand = prompt("Nhập tên hãng");
-        const category = prompt("Nhập tên loại");
-        if (name) {
-            this.setName = name;
-        }
-        if (price) {
-            this.setPrice = price;
-        }
-        if (category) {
-            this.setCategory = category;
-        }
-        if (color) {
-            this.setColor = color;
-        }
-        if (brand) {
-            this.setBrand = brand;
-        }
-    }
-    get displayData() {
-        return `Name: ${this.getName} Brand: ${this.getBrand} Price: ${this.getPrice} Color: ${this.getColor} Category: ${this.getCategory}`;
-    }
+const productElement = document.querySelector("#products-management .table tbody");
+const modalElement = document.querySelector("#exampleModal");
+function renderProduct(products) {
+    const content = products
+        .map((product, index) => `
+        <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>${product.quantity}</td>
+            <td>
+                <button class="btn btn-primary me-2" data-bs-toggle="modal"
+                data-bs-target="#exampleModal" onclick="handleEdit(${product.id})">Edit</button
+                ><button class="btn btn-danger"  onclick="handleDelete(${product.id})">Delete</button>
+            </td>
+        </tr>`)
+        .join("");
+    productElement.innerHTML = content;
 }
-class CarManager {
-    constructor() {
-        this.cars = [];
-    }
-    createCar() {
-        const car = new Car("", 0, "", "", "");
-        car.inputData();
-        this.cars.push(car);
-    }
-    updateCar(index) {
-        const car = this.cars[index];
-        car.inputData();
-        this.cars.splice(index, 1, car);
-    }
-    showAllCar() {
-        this.cars.forEach((car) => {
-            console.log(car.displayData);
-        });
-    }
-    deleteCar(index) {
-        this.cars.splice(index, 1);
-    }
+renderProduct(store.showAllProduct());
+function handleEdit(id) {
+    renderModal(id);
 }
-function handleCar() {
-    const selected = prompt("Nhập C/R/U/D để tạo, đọc, sửa hoặc xóa dữ liệu");
-    switch (selected) {
-        case "C":
-            myShowRoom.createCar();
-            break;
-        case "R":
-            myShowRoom.showAllCar();
-            break;
-        case "U":
-            const index = Number(prompt("Nhập vào vị trí xe muốn sửa"));
-            myShowRoom.updateCar(index);
-            break;
-        case "D":
-            const i = Number(prompt("Nhập vào vị trí xe muốn xóa"));
-            myShowRoom.deleteCar(i);
-            break;
-        default:
-            alert("Vui lòng nhập CRUD");
-            break;
+function handleDelete(id) {
+    store.deleteProduct(id);
+    renderProduct(store.showAllProduct());
+}
+function renderModal(id) {
+    console.log(id);
+    const product = store.showAllProduct().find((product) => product.id === id);
+    if (product) {
+        const content = `
+        <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">
+              Sửa sản phẩm
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <form>
+            <div class="mb-3">
+                <label for="id" class="form-label">ID</label>
+                <input
+                  disabled
+                  type="text"
+                  class="form-control"
+                  id="id"
+                  value="${id}"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  aria-describedby="emailHelp"
+                  value="${product.name}"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="price" class="form-label"
+                  >Price</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="price"
+                  value="${product.price}"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="quantity" class="form-label">Số lượng</label>
+                <input type="text" class="form-control" id="quantity"  value="${product.quantity}"/>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+      `;
+        modalElement.innerHTML = content;
     }
 }
